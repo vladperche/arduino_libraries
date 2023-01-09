ef91c2616bd8e255a0efa52bcdbbcf8ca031d236
714940 <714940@cognizant.com>
2023-01-09 09:39:57 -0300

    Date/Time format functions

--- VpcStrings/src/VpcStrings.cpp
+++ VpcStrings/src/VpcStrings.cpp
@@ -27,3 +27,33 @@ String VpcStrings::PadR(String source, char filler, int size)
     }
     return output;
 }
+
+String VpcStrings::DateToDDMMYYYY(int year, int month, int day)
+{
+    String value = VpcStrings->PadL(day, '0', 2);
+    value += "/";
+    value += VpcStrings->PadL(month, '0', 2);
+    value += "/";
+    value += VpcStrings->PadL(year, '0', 4);
+    return value;
+}
+
+String VpcStrings::DateToDDMMYY(int year, int month, int day)
+{
+    String value = VpcStrings->PadL(day, '0', 2);
+    value += "/";
+    value += VpcStrings->PadL(month, '0', 2);
+    value += "/";
+    value += VpcStrings->PadL(year % 100, '0', 2);
+    return value;
+}
+
+String VpcStrings::TimeToHHMMSS(int hour, int minutes, int seconds)
+{
+    String value = VpcStrings->PadL(hour, '0', 2);
+    value += ":";
+    value += VpcStrings->PadL(minutes, '0', 2);
+    value += ":";
+    value += VpcStrings->PadL(seconds, '0', 4);
+    return value;
+}
--- VpcStrings/src/VpcStrings.h
+++ VpcStrings/src/VpcStrings.h
@@ -14,6 +14,9 @@ public:
     VpcStrings();
     static String PadL(String source, char filler, int size);
     static String PadR(String source, char filler, int size);
+    static String DateToDDMMYYYY(int year, int month, int day);
+    static String DateToDDMMYY(int year, int month, int day);
+    static String TimeToHHMMSS(int hour, int minutes, int seconds);
 };
 
 #endif
